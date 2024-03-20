ed07e2a4e1b4cc86292616e07307c36b5c8a18c3
Vinícius Nogueira <vinicius.nogueira@agenciam3.com>
2024-03-20 17:11:13 -0300

    add project

---
+++ src/ts/index.ts
@@ -0,0 +1,9 @@
+import { Product } from "./Product";
+
+const serverUrl = "http://localhost:5000";
+
+function main() {
+  console.log(serverUrl);
+}
+
+document.addEventListener("DOMContentLoaded", main);
